ccd2b288128fa215801ba2d4af0613a1a354b83a
jose_miguel <josecastrillong@gmail.com>
2022-08-02 10:57:46 -0500

    installed eslint and created config file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
